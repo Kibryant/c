7a0bbf3e9fe1fe3b432b668ce0ec9d967c6b99ce
Kibryant <124307212+Kibryant@users.noreply.github.com>
2023-09-16 16:07:54 -0300

    fix

--- people.c
+++ people.c
@@ -12,21 +12,20 @@ struct People *fullName(char firstName[100], char lastName[100])
 {
   struct People *p = malloc(sizeof(struct People));
   strncpy(p->firstName, firstName, sizeof(p->firstName) - 1);
-  p->firstName[sizeof(p->firstName) - 1] = '\0'; // Certifique-se de terminar a string com '\0'
+  p->firstName[sizeof(p->firstName) - 1] = '\0'; 
 
   strncpy(p->lastName, lastName, sizeof(p->lastName) - 1);
-  p->lastName[sizeof(p->lastName) - 1] = '\0'; // Certifique-se de terminar a string com '\0'
+  p->lastName[sizeof(p->lastName) - 1] = '\0'; 
   return p;
 }
 
 char* name(struct People *p1, struct People *p2) {
-  // Tamanho total do nome é a soma dos tamanhos dos nomes mais um espaço para separação
   int totalSize = strlen(p1->firstName) + strlen(p1->lastName) + strlen(p2->firstName) + strlen(p2->lastName) + 3;
   
-  // Aloque memória para o nome completo
+  // malloc memory
   char* fullName = malloc(totalSize);
   
-  // Construa o nome completo concatenando os nomes
+
   snprintf(fullName, totalSize, "%s %s%s %s", p1->firstName, p1->lastName, p2->firstName, p2->lastName);
   
   return fullName;
@@ -40,7 +39,7 @@ int main() {
   
   printf("FUll NAME: %s\n", fullNameResult);
   
-  // Libere a memória alocada
+  // free memory
   free(person1);
   free(person2);
   free(fullNameResult);
