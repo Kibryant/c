f646a18fcf99a99ad254c8552d1968dd625a2f6d
Kibryant <arthurgustavon@gmail.com>
2023-11-22 15:08:13 -0300

    gg

---
+++ constants/main.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    const int NUMBER = 5;
+    printf("Number = %d\n", NUMBER);
+    const int RESULT = NUMBER + 5;
+    printf("Result = %d\n", RESULT);
+    return EXIT_SUCCESS;
+}
